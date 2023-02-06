1d4e15f876226ff9ec4a032082cb42b8f5d778c9
alesyaz <alesya.zarembo@mail.ru>
2023-02-06 19:03:34 +0300

    feat: implement track resource

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -3,36 +3,47 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  HttpStatus,
+  HttpCode,
+  ParseUUIDPipe,
+  Put,
 } from '@nestjs/common';
 import { TracksService } from './tracks.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/track.entity';
 
 @Controller('tracks')
 export class TracksController {
   constructor(private readonly tracksService: TracksService) {}
 
   @Post()
-  create(@Body() createTrackDto: CreateTrackDto) {
-    return this.tracksService.create(createTrackDto);
+  @HttpCode(HttpStatus.CREATED)
+  async createTrack(@Body() createTrackDto: CreateTrackDto): Promise<Track> {
+    return await this.tracksService.createTrack(createTrackDto);
   }
 
   @Get()
-  findAll() {
-    return this.tracksService.findAll();
+  @HttpCode(HttpStatus.OK)
+  async getTracks(): Promise<Track[]> {
+    return await this.tracksService.getTracks();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tracksService.findOne(+id);
+  @HttpCode(HttpStatus.OK)
+  async getTrack(@Param('id', ParseUUIDPipe) id: string): Promise<Track> {
+    return this.tracksService.getTrack(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTrackDto: UpdateTrackDto) {
-    return this.tracksService.update(+id, updateTrackDto);
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateTrackDto: UpdateTrackDto,
+  ) {
+    return await this.tracksService.updateTrack(id, updateTrackDto);
   }
 
   @Delete(':id')
--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -1,23 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { StoreService } from 'src/store/store.service';
+import { Track } from './entities/track.entity';
 
 @Injectable()
 export class TracksService {
-  create(createTrackDto: CreateTrackDto) {
-    return 'This action adds a new track';
+  async createTrack(createTrackDto: CreateTrackDto): Promise<Track> {
+    const track = {
+      id: uuidv4(),
+      ...createTrackDto,
+    };
+    StoreService.tracks.push(track);
+
+    return await track;
   }
 
-  findAll() {
-    return `This action returns all tracks`;
+  async getTracks(): Promise<Track[]> {
+    return StoreService.tracks;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} track`;
+  async getTrack(id: string): Promise<Track> {
+    const track = StoreService.tracks.find((track: Track) => track.id === id);
+
+    if (!track) {
+      throw new HttpException('Not found track', HttpStatus.NOT_FOUND);
+    }
+
+    return track;
   }
 
-  update(id: number, updateTrackDto: UpdateTrackDto) {
-    return `This action updates a #${id} track`;
+  async updateTrack(
+    id: string,
+    updateTrackDto: UpdateTrackDto,
+  ): Promise<Track> {
+    const track = StoreService.tracks.find((track) => track.id == id);
+
+    if (!track) {
+      throw new HttpException('Not found track', HttpStatus.NOT_FOUND);
+    }
+
+    track.name = updateTrackDto.name;
+    track.artistId = updateTrackDto.artistId;
+    track.albumId = updateTrackDto.albumId;
+    track.duration = updateTrackDto.duration;
+
+    const trackIndex = StoreService.tracks.findIndex((track) => track.id == id);
+
+    StoreService.tracks.splice(trackIndex, 1, track);
+
+    return track;
   }
 
   remove(id: number) {
